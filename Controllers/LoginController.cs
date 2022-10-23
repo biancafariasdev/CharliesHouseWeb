b18f67bf01c569955e97028f14a926c9495a915d
Bianca Farias <bianca.dcfarias@gmail.com>
2022-10-23 16:27:58 -0300

    Criação de tela de login • Realização de teste para validação de dados sem validação via banco.

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,39 @@
+﻿using CharliesHouseWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace CharliesHouseWeb.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Entrar (LoginModel loginModel)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+
+                    if(loginModel.Login == "adm" && loginModel.Senha == "123")
+                    {
+                        return RedirectToAction("Index", "Home");
+
+                    }
+                    TempData["MensagemErro"] = $"Ops! Não conseguimos seu login. Tente novament";
+
+                }
+                return View("Index");
+            }
+            catch (Exception erro)
+            {
+                TempData["MensagemErro"] = $"Ops! Não conseguimos seu login. Tente novamente. Detalhes do erro: {erro.Message}";
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
